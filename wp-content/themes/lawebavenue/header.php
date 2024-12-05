c625b57ed8921e088492d9c111d515121d3d4c7f
LWA-BU <lawebavenue@gmail.com>
2024-12-05 22:34:02 +0100

    correction images header

--- wp-content/themes/lawebavenue/header.php
+++ wp-content/themes/lawebavenue/header.php
@@ -34,33 +34,7 @@
                 
                 <h1 class="header__banner-title">artisan créateur d'espaces intérieurs <span class="header__banner-subtitle">sur mesure</span> </h1>
 
-                <div class="header__contact">
-
-                    <a class="header__contact-email" href="mailto:contact@lm-menuisier.com?subject=Demande d'information&body=Bonjour%20!%20Je%20souhaite%20avoir%20plus%20d'informations%20sur%20vos%20services." 
-                        target="_blank" 
-                        rel="noopener noreferrer" 
-                        aria-label="Envoyez-nous un message par email">
-                        <span class="sr-only">cliquez pour nous envoyer un message par email</span>
-                        <svg class="mail" width="32px" height="32px" viewBox="0 -4 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" fill="#000000"  aria-hidden="false" focusable="true" role="img" aria-labelledby="icon-title"><g id="SVGRepo_bgCarrier" stroke-width="0"></g> <title id="icon-title">Contacter par mail</title><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" sketch:type="MSPage"> <g id="Icon-Set" sketch:type="MSLayerGroup" transform="translate(-412.000000, -259.000000)" fill="#000000"> <path d="M442,279 C442,279.203 441.961,279.395 441.905,279.578 L433,270 L442,263 L442,279 L442,279 Z M415.556,280.946 L424.58,271.33 L428,273.915 L431.272,271.314 L440.444,280.946 C440.301,280.979 415.699,280.979 415.556,280.946 L415.556,280.946 Z M414,279 L414,263 L423,270 L414.095,279.578 C414.039,279.395 414,279.203 414,279 L414,279 Z M441,261 L428,271 L415,261 L441,261 L441,261 Z M440,259 L416,259 C413.791,259 412,260.791 412,263 L412,279 C412,281.209 413.791,283 416,283 L440,283 C442.209,283 444,281.209 444,279 L444,263 C444,260.791 442.209,259 440,259 L440,259 Z" id="mail" sketch:type="MSShapeGroup"> </path> </g> </g> </g></svg>
-                    </a>
-
-                    <a class="header__contact-whatsapp" href="https://wa.me/+352661346837?text=Bonjour%20!%20Je%20souhaite%20avoir%20plus%20d'informations%20sur%20vos%20services." 
-                        target="_blank" 
-                        rel="noopener noreferrer" 
-                        aria-label="Envoyez-nous un message sur WhatsApp">
-                        <span class="sr-only">Cliquez pour nous envoyer un message sur WhatsApp</span>
-                        <svg class="whatsapp" fill="#000000" width="32px" height="32px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" aria-hidden="false" focusable="true" role="img" aria-labelledby="icon-title"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><title id="icon-title">Contacter via WhatsApp</title><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><path d="M11.42 9.49c-.19-.09-1.1-.54-1.27-.61s-.29-.09-.42.1-.48.6-.59.73-.21.14-.4 0a5.13 5.13 0 0 1-1.49-.92 5.25 5.25 0 0 1-1-1.29c-.11-.18 0-.28.08-.38s.18-.21.28-.32a1.39 1.39 0 0 0 .18-.31.38.38 0 0 0 0-.33c0-.09-.42-1-.58-1.37s-.3-.32-.41-.32h-.4a.72.72 0 0 0-.5.23 2.1 2.1 0 0 0-.65 1.55A3.59 3.59 0 0 0 5 8.2 8.32 8.32 0 0 0 8.19 11c.44.19.78.3 1.05.39a2.53 2.53 0 0 0 1.17.07 1.93 1.93 0 0 0 1.26-.88 1.67 1.67 0 0 0 .11-.88c-.05-.07-.17-.12-.36-.21z"></path><path d="M13.29 2.68A7.36 7.36 0 0 0 8 .5a7.44 7.44 0 0 0-6.41 11.15l-1 3.85 3.94-1a7.4 7.4 0 0 0 3.55.9H8a7.44 7.44 0 0 0 5.29-12.72zM8 14.12a6.12 6.12 0 0 1-3.15-.87l-.22-.13-2.34.61.62-2.28-.14-.23a6.18 6.18 0 0 1 9.6-7.65 6.12 6.12 0 0 1 1.81 4.37A6.19 6.19 0 0 1 8 14.12z"></path></g></svg>
-                    </a>
-
-                    <a class="header__contact-phone" href="tel:+352661346837" 
-                        target="_blank" 
-                        rel="noopener noreferrer" 
-                        aria-label="Appelez-nous">
-                        <span class="sr-only">Cliquer pour nous appeler par téléphone</span>
-                        <svg class="tel"width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"  aria-hidden="false" focusable="true" role="img" aria-labelledby="icon-title"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><title id="icon-title">Contacter par téléphone</title><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path stroke="#000000" d="M14.05 6C15.0268 6.19057 15.9244 6.66826 16.6281 7.37194C17.3318 8.07561 17.8095 8.97326 18 9.95M14.05 2C16.0793 2.22544 17.9716 3.13417 19.4163 4.57701C20.8609 6.01984 21.7721 7.91101 22 9.94M18.5 21C9.93959 21 3 14.0604 3 5.5C3 5.11378 3.01413 4.73086 3.04189 4.35173C3.07375 3.91662 3.08968 3.69907 3.2037 3.50103C3.29814 3.33701 3.4655 3.18146 3.63598 3.09925C3.84181 3 4.08188 3 4.56201 3H7.37932C7.78308 3 7.98496 3 8.15802 3.06645C8.31089 3.12515 8.44701 3.22049 8.55442 3.3441C8.67601 3.48403 8.745 3.67376 8.88299 4.05321L10.0491 7.26005C10.2096 7.70153 10.2899 7.92227 10.2763 8.1317C10.2643 8.31637 10.2012 8.49408 10.0942 8.64506C9.97286 8.81628 9.77145 8.93713 9.36863 9.17882L8 10C9.2019 12.6489 11.3501 14.7999 14 16L14.8212 14.6314C15.0629 14.2285 15.1837 14.0271 15.3549 13.9058C15.5059 13.7988 15.6836 13.7357 15.8683 13.7237C16.0777 13.7101 16.2985 13.7904 16.74 13.9509L19.9468 15.117C20.3262 15.255 20.516 15.324 20.6559 15.4456C20.7795 15.553 20.8749 15.6891 20.9335 15.842C21 16.015 21 16.2169 21 16.6207V19.438C21 19.9181 21 20.1582 20.9007 20.364C20.8185 20.5345 20.663 20.7019 20.499 20.7963C20.3009 20.9103 20.0834 20.9262 19.6483 20.9581C19.2691 20.9859 18.8862 21 18.5 21Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-                    </a>
-
-                </div>
+                <?php get_template_part('parts/contact_banner'); ?>
 
                 <div class="header__cta">
                     <button class="header__cta-link header__cta-link-cuisines">
@@ -75,6 +49,36 @@
 
                     <button class="header__cta-link header__cta-link-dressings">
                         <span>Dressings</span>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow-right.png" alt="" >
+                    </button>
+                </div>
+
+            </div>
+
+        <?php } ?>
+
+
+        <?php if(is_page('prestations')){ ?>
+
+            <div class="headerPrestations__banner">
+                
+                <h1 class="headerPrestations__banner-title">Prestations </h1>
+
+                <?php get_template_part('parts/contact_banner'); ?>
+
+                <div class="headerPrestationsr__cta-">
+                    <button class="header__cta-link header__cta-link">
+                        <span></span>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow-right.png" alt="" >
+                    </button>
+
+                    <button class="headerPrestations__cta-link header__cta-link">
+                        <span></span>
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow-right.png" alt="" >
+                    </button>
+
+                    <button class="headerPrestations__cta-link header__cta-link">
+                        <span></span>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow-right.png" alt="" >
                     </button>
                 </div>
