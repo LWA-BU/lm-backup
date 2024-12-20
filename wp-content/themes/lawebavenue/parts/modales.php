a6afc7dc7202799e3f18ccdd3c2c3e09f7f02164
LWA-BU <lawebavenue@gmail.com>
2024-12-20 19:34:33 +0100

    mise en place d'un carroussel dans la modale

--- wp-content/themes/lawebavenue/parts/modales.php
+++ wp-content/themes/lawebavenue/parts/modales.php
@@ -84,3 +84,82 @@
 
 
 
+
+
+
+  <section class="modal modal-design" id="modal2" role="dialog" aria-modal="true" aria-hidden="true" aria-labelledby="modal1-title">
+
+<div class="modal__content">
+  <button class="close-modal modal__btn" tabindex="0" aria-label="Fermer la modale" >
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icones/cross.svg"  alt="">
+  </button>
+  <p class="modal__subtitle">Conception, réalisation, pose</p>
+  <h1 class="modal__title section-title">Revêtement de sol</h1>
+  <p class="modal__p paragraphe">
+    revêtement de sol
+  </p>
+
+  <div class="modal__gallery">
+
+    <div class="modal__gallery-item">
+      <div class="modal__gallery-img-wrapper">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prestations/agencement-cuisine.jpeg" alt="" class="modal__gallery-img">
+      </div>
+      
+      <h2 class="modal__gallery-title">Cuisine</h2>
+    </div>
+
+    <div class="modal__gallery-item">
+      <div class="modal__gallery-img-wrapper">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prestations/agencement-salle-de-bain.jpg"  alt="" class="modal__gallery-img">
+      </div>
+      <h2 class="modal__gallery-title">Salle de bains</h2>
+    </div>
+
+    <div class="modal__gallery-item">
+      <div class="modal__gallery-img-wrapper">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prestations/agencement-dressing.jpg"  alt="" class="modal__gallery-img">
+      </div>
+      <h2 class="modal__gallery-title">Dressing</h2>
+    </div>
+
+    <div class="modal__gallery-item">
+      <div class="modal__gallery-img-wrapper">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prestations/agencement-placard.jpg"  alt="" class="modal__gallery-img">
+      </div>
+      <h2 class="modal__gallery-title">Aménagement placard</h2>
+    </div>
+
+    <div class="modal__gallery-item">
+      <div class="modal__gallery-img-wrapper">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prestations/agencement-buanderie-cave.jpg"  alt="" class="modal__gallery-img">
+      </div>
+      <h2 class="modal__gallery-title">Buanderie / Cave</h2>
+    </div>
+
+    <div class="modal__gallery-item">
+      <div class="modal__gallery-img-wrapper">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prestations/agencement-etagere-bibliotheque.jpg"  alt="" class="modal__gallery-img">
+      </div>
+      <h2 class="modal__gallery-title">Etagères<br>Bibliothèque </h2>
+    </div>
+
+    <div class="modal__gallery-item">
+      <div class="modal__gallery-img-wrapper">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prestations/agencement-meubleTV.jpg"  alt="" class="modal__gallery-img">
+      </div>
+      <h2 class="modal__gallery-title">Ensembles intégrés <br>Meuble TV</h2>
+    </div>
+
+    <div class="modal__gallery-item">
+      <div class="modal__gallery-img-wrapper">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/prestations/agencement-tablette-fenetre.jpg"  alt="" class="modal__gallery-img">
+      </div>
+      <h2 class="modal__gallery-title">Tablette de fenêtre</h2>
+    </div>
+
+  </div>
+
+</div>
+
+</section>
