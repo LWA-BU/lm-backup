80370c2edf90ea83abab027acd3cb2c430843dfb
LWA-BU <lawebavenue@gmail.com>
2025-01-29 22:41:49 +0100

    mise en place section combine

--- wp-content/themes/lawebavenue/templates/page-agencement-interieur.php
+++ wp-content/themes/lawebavenue/templates/page-agencement-interieur.php
@@ -48,8 +48,54 @@ get_header(); ?>
                     <p class="agencement-etagere__p paragraphe">
                         Bibliothèque et étagère permettent un rangement pratique et esthétique. Elles optimisent l’espace en libérant les surfaces et apportent un aspect structuré et soigné à la pièces.  
                     </p>
-       
             </div>
+        </section>
+
+        <div class="agencement__icones">
+            <span class="agencement__icones-kitchen icone"></span>
+            <span class="agencement__icones-bathroom icone"></span>
+            <span class="agencement__icones-dressing icone"></span>
+            <span class="agencement__icones-library icone"></span>
+        </div>
+
+        <section class="agencement__combine">
+
+            <div class="agencement__combine-img">
+
+                <div class="agencement__combine-img-rect">
+                    <picture class="agencement__combine-img-small">
+                        <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/meuble-tv-blanc-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/meuble-tv-blanc.jpg" alt="Bibliothèque en mélaminé blanc sur bureau avec tiroirs et plan en marbre" width="395" 
+                        height="273">
+                    </picture>    
+
+                    <picture class="agencement__combine-img-square">
+                        <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/meuble-tv-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/meuble-tv.jpg" alt="Tablettes de cuisine en bois de chêne clair" width="468" 
+                        height="345">
+                    </picture>    
+                </div>
+
+                <picture class="agencement__combine-img-tall">
+                    <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/combine-TV-encastre-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/combine-TV-encastre.jpg" alt="Bibliotheque chic en bois gris anthracite au hauts arrondis et bas avec portes" width="400" 
+                    height="566">
+                </picture>  
+            </div>
+            
+            <div class="agencement__combine-content">
+                <span class="agencement__combine-trait"></span>
+                <div class="agencment__combine-txt">
+                    <p class="agencement__combine-subtitle section-subtitle">Organiser son salon</p>
+                    <h1 class="agencement__combine-title agencement-title">Meuble TV combiné</h1>
+                    <p class="agencement__combine-p paragraphe">
+                        Les meubles TV combinés sont pratiques et esthétiques : ils offrent un espace de rangement pour organiser les équipements multimédias. 
+                        En plus d’être fonctionnels, ils apportent une touche décorative au salon, s’adaptent aux différents styles d’intérieur grâce à une large variété de designs et de matériaux.
+                    </p>
+                </div>
+            </div>
+     
+
         </section>
 
     </main>
