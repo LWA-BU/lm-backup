ac2534e7c5eadda311269d696d94ac8c8ac1a142
LWA-BU <lawebavenue@gmail.com>
2025-01-31 19:24:43 +0100

    mise en place block etagere

--- wp-content/themes/lawebavenue/templates/page-agencement-interieur.php
+++ wp-content/themes/lawebavenue/templates/page-agencement-interieur.php
@@ -18,37 +18,41 @@ get_header(); ?>
         </div>
 
         <section class="agencement-etagere">
-
+  
             <div class="agencement-etagere__main">
 
-                    <h1 class="agencement-etagere__title agencement-title">étagère & bibliothèque</h1>
+                <h1 class="agencement-etagere__title agencement-title">étagère & bibliothèque</h1>
 
-                    <div class="agencement-etagere__img">
-                        <picture class="agencement-etagere__img-small">
-                            <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bibliotheque-etagere-480.jpg" media="(max-width: 480px)" type"image/jpg">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bibliotheque-etagere.jpg" alt="Bibliothèque en mélaminé blanc sur bureau avec tiroirs et plan en marbre" width="375" 
-                            height="440">
-                        </picture>    
+                <div class="agencement-etagere__img">
 
+                    <picture class="agencement-etagere__img-small img480">
+                        <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bibliotheque-etagere-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bibliotheque-etagere.jpg" alt="Bibliothèque en mélaminé blanc sur bureau avec tiroirs et plan en marbre" width="835" 
+                        height="925">
+                    </picture>    
+
+                    <div class="agencement-etagere__img-txt">
                         <picture class="agencement-etagere__img-tall">
                             <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bibliotheque-480.jpg" media="(max-width: 480px)" type"image/jpg">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bibliotheque.jpg" alt="Bibliotheque chic en bois gris anthracite au hauts arrondis et bas avec portes" width="385" 
-                            height="600">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bibliotheque.jpg" alt="Bibliotheque chic en bois gris anthracite au hauts arrondis et bas avec portes" width="600" 
+                            height="943">
                         </picture>    
-
-                        <picture class="agencement-etagere__img-square">
-                            <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/etagere-bois-480.jpg" media="(max-width: 480px)" type"image/jpg">
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/etagere-bois.jpg" alt="Tablettes de cuisine en bois de chêne clair" width="375" 
-                            height="375">
-                        </picture>    
+                        <p class="agencement-etagere__p paragraphe">
+                            Bibliothèque et étagère permettent un rangement pratique et esthétique. Elles optimisent l’espace en libérant les surfaces et apportent un aspect structuré et soigné à la pièces.  
+                        </p>
                     </div>
 
-                    <span class="agencement-etagere__banner"></span>
+                    <picture class="agencement-etagere__img-square">
+                        <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/etagere-bois-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/etagere-bois.jpg" alt="Tablettes de cuisine en bois de chêne clair" width="600" 
+                        height="624">
+                    </picture>    
 
-                    <p class="agencement-etagere__p paragraphe">
-                        Bibliothèque et étagère permettent un rangement pratique et esthétique. Elles optimisent l’espace en libérant les surfaces et apportent un aspect structuré et soigné à la pièces.  
-                    </p>
+                </div>
+                
             </div>
+
+            <span class="agencement-etagere__banner"></span>
         </section>
 
         <div class="agencement__icones">
@@ -65,27 +69,27 @@ get_header(); ?>
                 <div class="agencement__combine-img-rect">
                     <picture class="agencement__combine-img-small">
                         <source srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/meuble-tv-blanc-480.jpg" media="(max-width: 480px)" type"image/jpg">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/meuble-tv-blanc.jpg" alt="Bibliothèque en mélaminé blanc sur bureau avec tiroirs et plan en marbre" width="395" 
-                        height="273">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/meuble-tv-blanc.jpg" alt="Bibliothèque en mélaminé blanc sur bureau avec tiroirs et plan en marbre" width="700" 
+                        height="526">
                     </picture>    
 
                     <picture class="agencement__combine-img-square">
                         <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/meuble-tv-480.jpg" media="(max-width: 480px)" type"image/jpg">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/meuble-tv.jpg" alt="Tablettes de cuisine en bois de chêne clair" width="468" 
-                        height="345">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/meuble-tv.jpg" alt="Tablettes de cuisine en bois de chêne clair" width="700" 
+                        height="477">
                     </picture>    
                 </div>
 
                 <picture class="agencement__combine-img-tall">
                     <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/combine-TV-480.jpg" media="(max-width: 767px)" type"image/jpg">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/combine-TV.jpg" alt="Bibliotheque chic en bois gris anthracite au hauts arrondis et bas avec portes" width="400" 
-                    height="566">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/combine-TV.jpg" alt="Bibliotheque chic en bois gris anthracite au hauts arrondis et bas avec portes" width="500" 
+                    height="750">
                 </picture>  
             </div>
             
             <div class="agencement__combine-content">
                 <span class="agencement__combine-trait"></span>
-                <div class="agencment__combine-txt">
+                <div class="agencement__combine-txt">
                     <p class="agencement__combine-subtitle section-subtitle">Organiser son salon</p>
                     <h1 class="agencement__combine-title agencement-title">Meuble TV & combiné</h1>
                     <p class="agencement__combine-p paragraphe">
@@ -94,8 +98,105 @@ get_header(); ?>
                     </p>
                 </div>
             </div>
-     
 
+        </section>
+
+        <div class="agencement__contact">
+            <div class="agencement__contact-title">
+                <span class="agencement__icones-projet icone"></span>
+                <h2 class="agencement__contact-">Vous avez un projet ?</h2>
+            </div>
+
+            <p class="agencement__contact-p">
+                Vous souhaitez créer un nouvel espace, ajouter un nouveau meuble sur mesure, chez vous ou dans vos bureaux ? 
+                N’hésitez pas à contacter LM Menuisier pour étudier votre projet. 
+            </p>
+
+            <a class="btn-transparent-black"  href="votre-lien.html" aria-label="Contactez-nous">
+                <span>contactez-nous</span>
+                <span class="section-concept__button-icon icone" aria-hidden="true"></span>
+            </a>
+          
+        </div>
+
+        <section class="agencement__chambre">
+            <div class="agencement__chambre-txt">
+                <div class="agencement__chambre-main-title">
+                    <p class="agencement__chambre-subtitle section-subtitle">Un espace pour se ressourcer</p>
+                    <h1 class="agencement__chambre-title agencement-title">Chambre & bureau</h1>
+                </div>
+                <p class="agencement__chambre-p paragraphe">
+                    « Les meubles de chambre sur mesure répondent à vos habitudes et besoins pratiques. 
+                    Leur conception entièrement personnalisable permet d’intégrer des solutions astucieuses pour optimiser le rangement ou améliorer l’ergonomie, 
+                    contribuant ainsi à un environnement plus ordonné et apaisant.»
+                </p>
+            </div>
+
+            <div class="agencement__chambre-img">
+
+                <div class="agencement__chambre-img-col1">
+                    <picture class="agencement__chambre-img-tall">
+                        <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/kids-room-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/kids-room.jpg" alt="chambre d'enfant avec lits superposés" width="900" 
+                        height="1216">
+                    </picture>    
+
+                    <picture class="agencement__chambre-img-small">
+                        <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/desk-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/desk.jpg" alt="Bureau en mélaminé blanc avec module quatre tiroirs" width="900" 
+                        height="620">
+                    </picture>   
+                </div>
+
+                <div class="agencement__chambre-img-col2">
+                    <picture class="agencement__chambre-img-rect">
+                        <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bed-and-desk-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bed-and-desk.jpg" alt="Lit d'adolescent surélevé sur tiroir avec bureau intégré" width="940" 
+                        height="529">
+                    </picture>    
+
+                    <picture class="agencement__chambre-img-rect">
+                        <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bed-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/bed.jpg" alt="Bureau et bibliothèque en mélaminé blanc dans chambre d'enfant" width="940" 
+                        height="694">
+                    </picture>   
+                </div>
+
+            </div>
+
+        </section>
+
+
+        <section class="agencement__buanderie">
+            <p class="agencement__buanderie-txt">Optimiser les espaces</p>
+           
+            <div class="agencement__buanderie-txt">
+                <div class="agencement__buanderie-main-title">
+                    <p class="agencement__buanderie-subtitle section-subtitle">Optimiser chaque espace</p>
+                    <h1 class="agencement__buanderie-title agencement-title">Buanderie & cave</h1>
+                    <span class="agencement__buanderie-trait"></span>
+                </div>
+                <p class="agencement__buanderie-p paragraphe">
+                    « Optimisez chaque espace de votre maison avec une buanderie fonctionnelle ou une cave à vin. La buanderie, pensée pour maximiser le rangement et simplifier les tâches ménagères, 
+                    peut être aménagée avec des solutions ergonomiques. 
+                    Pour les amateurs de vins, une cave sur mesure assure une conservation optimale. Nous vous accompagnons dans la création d’espaces à la fois pratiques et esthétiques, 
+                    alliant design et fonctionnalité pour répondre à vos besoins quotidiens. »
+                </p>
+            </div>
+
+            <div class="agencement__buanderie-img">
+                <picture class="agencement__buanderie-img-tall">
+                    <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/buanderie-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/buanderie.jpg" alt="Buanderie sur mesure meubles couleurs chêne et bois" width="700" 
+                    height="900">
+                </picture>    
+
+                <picture class="agencement__buanderie-img-tall">
+                    <source  srcset="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/cave-a-vin-480.jpg" media="(max-width: 480px)" type"image/jpg">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/agencement/cave-a-vin.jpg" alt="Cave à vin sur mesure en bois de chêne foncé" width="680" 
+                    height="916">
+                </picture>   
+            </div>
         </section>
 
     </main>
